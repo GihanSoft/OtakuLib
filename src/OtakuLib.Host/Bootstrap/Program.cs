e87137f5c2d3e3d57aaec01f58d843edf93e9785
Mohammad Babayi <chief.mb1976@gmail.com>
2021-10-15 15:44:33 +0330

    application template

---
+++ src/OtakuLib.Host/Bootstrap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using OtakuLib.View;
+
+namespace OtakuLib.Host.Bootstrap
+{
+    public static class Program
+    {
+        [STAThread]
+        public static int Main()
+        {
+            App app = new();
+            app.InitializeComponent();
+
+            Win win = new();
+
+            return app.Run(win);
+        }
+    }
+}
