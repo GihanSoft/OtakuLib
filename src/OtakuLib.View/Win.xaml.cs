817a7238e3e36b0310383a58635e49ef1a6f9896
Mohammad Babayi <chief.mb1976@gmail.com>
2021-10-16 06:57:23 +0330

    setup mahapps

--- src/OtakuLib.View/Win.xaml.cs
+++ src/OtakuLib.View/Win.xaml.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace OtakuLib.View
+﻿namespace OtakuLib.View
 {
     /// <summary>
     /// Interaction logic for Win.xaml
     /// </summary>
-    public partial class Win : Window
+    public sealed partial class Win
     {
         public Win()
         {
