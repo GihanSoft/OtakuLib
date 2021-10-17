6297f13fe1e150925ceb6a9dfd7b28ea9e63c0f8
Mohammad Babayi <chief.mb1976@gmail.com>
2021-10-17 06:30:43 +0330

    Set Win Title

--- src/OtakuLib.Logic/ViewModels/WinVM.cs
+++ src/OtakuLib.Logic/ViewModels/WinVM.cs
@@ -6,7 +6,7 @@ namespace OtakuLib.Logic.ViewModels
 {
     internal sealed class WinVM : ViewModelBase, IWinVM
     {
-        private string title = string.Empty;
+        private string title = "OtakuLib";
 
         public string Title
         {
