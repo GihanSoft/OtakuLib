d8ac93185e51dd064dd00fcc3ca2b7461524da75
Muhammad Babayi <chief.mb1976@gmail.com>
2022-01-04 08:19:34 +0330

    add save state to main window + fix minimize

--- src/OtakuLib.WFP/Win.xaml.cs
+++ src/OtakuLib.WFP/Win.xaml.cs
@@ -1,4 +1,10 @@
-﻿using OtakuLib.WPF.ViewModels;
+﻿using System.ComponentModel;
+using System.Windows.Threading;
+
+using GihanSoft.AppBase.Services;
+
+using OtakuLib.WPF.Models.Data;
+using OtakuLib.WPF.ViewModels;
 
 namespace OtakuLib.WPF;
 
@@ -8,10 +14,32 @@ namespace OtakuLib.WPF;
 [CLSCompliant(false)]
 public sealed partial class Win
 {
-    public Win(WinVM viewModel)
+    private readonly IDataManager<WinState> dataProvider;
+    private WindowState validWindowState;
+
+    public Win(WinVM viewModel, IDataManager<WinState> dataProvider)
     {
+        this.dataProvider = dataProvider;
         ViewModel = viewModel;
+
         InitializeComponent();
+        Loaded += OnLoaded;
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs args)
+    {
+        if (!dataProvider.TryFetch(out var state) || state is null)
+        {
+            return;
+        }
+
+        SetCurrentValue(LeftProperty, state.Left);
+        SetCurrentValue(TopProperty, state.Top);
+        SetCurrentValue(WidthProperty, state.Width);
+        SetCurrentValue(HeightProperty, state.Height);
+        SetCurrentValue(WindowStateProperty, state.WindowState);
+
+        validWindowState = state.WindowState;
     }
 
     public WinVM ViewModel { get; }
@@ -20,5 +48,28 @@ public sealed partial class Win
     {
         ViewModel.IsBackStackOpen = false;
         ViewModel.IsForwardStackOpen = false;
+    }
+
+    protected override void OnStateChanged(EventArgs e)
+    {
+        base.OnStateChanged(e);
+        if (WindowState is not WindowState.Minimized)
+        {
+            validWindowState = WindowState;
+        }
+    }
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+        base.OnClosing(e);
+
+        dataProvider.Save(new WinState
+        {
+            Left = RestoreBounds.Left,
+            Top = RestoreBounds.Top,
+            Width = RestoreBounds.Width,
+            Height = RestoreBounds.Height,
+            WindowState = validWindowState,
+        });
     }
 }
